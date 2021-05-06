a4a922cee947d7894328d80f575ce217e472bac0
Ana Marija Belingar <ana.belingar@gmail.com>
2021-05-06 13:22:29 +0200

    b

---
+++ priklop.py
@@ -0,0 +1,30 @@
+import psycopg2, psycopg2.extensions, psycopg2.extras
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
+
+import csv
+
+from auth import *
+
+#UVOZ SQL SKRIPTE
+def uvoziSQL(cur, datoteka):
+    with open(datoteka) as f:
+        skripta = f.read()
+        cur.execute(skripta)
+
+def uvoziCSV(cur, tabela):
+    with open('obdelani-podatki/{0}.csv'.format(tabela), encoding='utf-8') as csvfile:
+        podatki = csv.reader(csvfile)
+        vsiPodatki = [vrstica for vrstica in podatki]
+        glava = vsiPodatki[0]
+        vrstice = vsiPodatki[1:]
+        cur.executemany("INSERT INTO {0} ({1}) VALUES ({2})".format(
+        tabela, ",".join(glava), ",".join(['%s']*len(glava))), vrstice)
+
+with psycopg2.connect(database=db, host=host, user=user, password=password) as con:
+    cur = con.cursor()
+    uvoziSQL(cur, 'ogrodje_tabel.sql')
+    uvoziCSV(cur, 'knjige')
+    # uvoziCSV(cur, 'agencije')
+    # uvoziNepremicnineCSV(cur, 'nepremicnine')
+    # uvoziCSV(cur, 'uporabniki')
+    con.commit()
